774c2a27a5025923aae4a949d490a0988b9c4b22
buithimaixuan <xuanbui2207@gmail.com>
2025-05-17 14:33:19 +0700

    call api friend page

--- src/components/Friend/FriendSidebar.js
+++ src/components/Friend/FriendSidebar.js
@@ -1,23 +1,47 @@
 import React from "react";
 
-const FriendSidebar = () => {
+const FriendSidebar = ({ setSection }) => {
   return (
-    <div className="w-[289px] h-screen bg-[#E5E4E9] font-roboto rounded-[10px] hidden md:block">
+    <div className="w-[289px] h-screen bg-[#E5E4E9] font-roboto rounded-r-[10px]  hidden md:block">
       <div className="ml-8 pt-6 flex items-start">
         <p className="text-lg font-bold ">FRIEND PAGE</p>
       </div>
       <div className="mx-8 mt-14 w-[225px] h-[155px] flex flex-col gap-4">
-        <button className="hover:bg-[#999999] flex w-full h-10 rounded-[10px]">
+        <button
+          onClick={() => setSection("list-follower")}
+          className="hover:bg-[#999999] flex w-full h-10 rounded-[10px]"
+        >
           <div className="mx-4 flex items-center">
             <i class="fa-solid fa-user-group"></i>
           </div>
-          <div className="font-bold flex items-center">Friend request</div>
+          <div className="font-bold flex items-center">Your follower</div>
         </button>
-        <button className="hover:bg-[#999999] flex w-full h-10 rounded-[10px]">
-          <div className="mx-4">
+        <button
+          onClick={() => setSection("list-following")}
+          className="hover:bg-[#999999] flex w-full h-10 rounded-[10px]"
+        >
+          <div className="mx-4 flex items-center">
             <i class="fa-solid fa-user-plus"></i>
           </div>
-          <div className="font-bold">Friend request</div>
+          <div className="font-bold flex items-center">Your following</div>
+        </button>
+        <button
+          onClick={() => setSection("list-friend")}
+          className="hover:bg-[#999999] flex w-full h-10 rounded-[10px]"
+        >
+          <div className="mx-4 flex items-center">
+            <i class="fa-solid fa-user-plus"></i>
+          </div>
+          <div className="font-bold flex items-center">Your friends</div>
+        </button>
+        <button
+          onClick={() => setSection("list-follower")}
+          className="hover:bg-[#999999] flex w-full h-10 rounded-[10px]"
+        >
+           <div className="mx-4 flex items-center">
+            <i class="fa-solid fa-user-plus"></i>
+          </div>
+          <div className="font-bold flex items-center">Friend suggestions</div>
         </button>
       </div>
     </div>
