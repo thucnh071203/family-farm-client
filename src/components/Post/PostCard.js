3ad087bd83d2c57e840415d6bdbfed77e64af5d8
buithimaixuan <xuanbui2207@gmail.com>
2025-05-19 18:16:07 +0700

    add option post, post in group page

--- src/components/Post/PostCard.js
+++ src/components/Post/PostCard.js
@@ -1,9 +1,11 @@
 import React from "react";
-
+import MoreIcon from "../../assets/images/more_horiz.svg";
+import OptionsPost from "./OptionsPost";
 const PostCard = ({ post }) => {
   const defaultPost = {
     username: "Phuong Nam",
-    avatar: "https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Minecraft_2024_cover_art.png/250px-Minecraft_2024_cover_art.png",
+    avatar:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Minecraft_2024_cover_art.png/250px-Minecraft_2024_cover_art.png",
     timestamp: "July 29, 2018, 07:49 AM",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. #blog #nienmoulming #polytecode",
@@ -13,21 +15,48 @@ const PostCard = ({ post }) => {
     shares: 10,
   };
   const postData = { ...defaultPost, ...post };
-
+  const hashTag = ["blog", "nienmoulming", "polytecode"];
+  const category = ["Pants", "Diseases"];
   return (
     <div className="bg-white p-5 rounded-lg shadow-md mb-5">
-      <div className="flex items-center gap-3 mb-3">
-        <img
-          src={postData.avatar}
-          alt="Avatar"
-          className="w-10 h-10 rounded-full"
-        />
+      <div className="flex justify-between">
+        <div className="flex items-center gap-3 mb-3">
+          <img
+            src={postData.avatar}
+            alt="Avatar"
+            className="w-10 h-10 rounded-full"
+          />
+          <div>
+            <h3 className="font-bold">{postData.username}</h3>
+            <p className="text-sm text-gray-500">{postData.timestamp}</p>
+          </div>
+        </div>
+        {/* add icon "..." */}
         <div>
-          <h3 className="font-bold">{postData.username}</h3>
-          <p className="text-sm text-gray-500">{postData.timestamp}</p>
+          <div>
+            <OptionsPost />
+          </div>
         </div>
       </div>
-      <p className="mb-3">{postData.content}</p>
+      <div className="flex flex-col items-start mt-3 text-sm">
+        <p className="mb-3 text-[#7D7E9E] font-light">{postData.content}</p>
+        <p className="mb-3 font-bold">
+          {hashTag.map((tag, index) => (
+            <span key={index} className="mr-2">
+              #{tag}
+            </span>
+          ))}
+        </p>
+        <p className="mb-3 flex">
+          <p className="font-bold"> Category:</p>
+          {category.map((cat, index) => (
+            <span key={index} className="mr-2">
+              {cat}
+              {index < category.length - 1 ? "," : ""}
+            </span>
+          ))}
+        </p>
+      </div>
       {postData.images && postData.images.length > 0 && (
         <>
           {/* Trường hợp có đúng 3 ảnh */}
@@ -55,13 +84,13 @@ const PostCard = ({ post }) => {
             // Các trường hợp khác: 1-2 ảnh, 4+ ảnh
             <div className="grid grid-cols-2 gap-2 mb-3">
               {postData.images.slice(0, 4).map((img, index) => {
-                const isLastVisible =
-                  index === 3 && postData.images.length > 4;
+                const isLastVisible = index === 3 && postData.images.length > 4;
                 return (
                   <div
                     key={index}
-                    className={`relative rounded-md overflow-hidden ${postData.images.length === 1 ? "col-span-2" : ""
-                      }`}
+                    className={`relative rounded-md overflow-hidden ${
+                      postData.images.length === 1 ? "col-span-2" : ""
+                    }`}
                   >
                     <img
                       src={img}
@@ -84,13 +113,15 @@ const PostCard = ({ post }) => {
       <div className="flex items-center justify-between">
         <div className="flex gap-3">
           <p>
-            <i className="fa-solid fa-thumbs-up text-blue-500"></i> {postData.likes}
+            <i className="fa-solid fa-thumbs-up text-blue-500"></i>{" "}
+            {postData.likes}
           </p>
           <p>
             <i className="fas fa-comment text-blue-500"></i> {postData.comments}
           </p>
           <p>
-            <i className="fa-solid fa-share text-blue-500"></i> {postData.shares}
+            <i className="fa-solid fa-share text-blue-500"></i>{" "}
+            {postData.shares}
           </p>
         </div>
         <div className="flex gap-3">
