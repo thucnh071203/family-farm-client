473b4ec933034f6f3a742e933b76e9f8c7f40b68
buithimaixuan <xuanbui2207@gmail.com>
2025-05-28 23:10:35 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,44 +1,73 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 import Register from "./components/Register/Register";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import HomePage from "./pages/HomePage/HomePage";
 import ServicePage from "./pages/ServicePage/ServicePage";
-import FriendRight from "./components/Friend/FriendRight";
+import ProcessListPage from "./pages/ProcessPage/ProcessListPage";
+import ProcessListFarmerPage from "./pages/ProcessPage/ProgressListFarmerPage";
+import CreateStepPage from "./pages/ProcessPage/CreateStepPage";
+import SavedPostPage from "./pages/SavedPostPage/SavedPostPage";
+import WaitingListPage from "./pages/WaitingPage/WaitingListPage";
 import FriendPage from "./pages/FriendPage/FriendPage";
-import FriendSidebar from "./components/Friend/FriendSidebar";
 import ServiceManagement from "./components/ServiceManagement/ServiceManagement";
-import CreateProgessStep from "./components/ProgressStep/CreateProgessStep";
+import CreateProcessStep from "./components/ProcessStep/CreateProcessStep";
+import { Statistic1 } from "./components/Statistic/Statistic1";
+import MapChart from "./components/Statistic/MapChart";
+import { UserGrowthChart } from "./components/Statistic/UserGrowthChart";
 import PersonalPage from "./pages/Profile/PersonalPage";
-import PostInGroupCard from "./components/Group/PostInGroupCard";
-
-import { Toaster } from "sonner";
-import PostCard from "./components/Post/PostCard";
-import OptionsPost from "./components/Post/OptionsPost";
-import PostInGroupRight from "./components/Group/PostInGroupRight";
+import UpdateProfile from "./pages/Profile/UpdateProfile";
+import UserFriends from "./pages/Profile/UserFriends";
 import PostGroupPage from "./pages/GroupPage/PostGroupPage";
-import YourGroupCard from "./components/Group/YourGroupCard";
+import CreateServicePage from "./pages/ServicePage/CreateServicePage";
+import ServiceDetailPage from "./pages/ServicePage/ServiceDetailPage";
+import ProcessResultPage from "./pages/ProcessPage/ProcessResultPage";
 
 function App() {
   return (
     <div className="App">
-      {/* vị trí của toast  */}
-      <Toaster position="top-right" />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        closeOnClick={true}
+        pauseOnHover={true}
+        draggable={true}
+        theme="colored"
+        richColors
+      />
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/Register" element={<Register />} />
           <Route path="/ServiceManagement" element={<ServiceManagement />} />
-          <Route path="/login" element={<LoginPage />} />
+          <Route path="/Login" element={<LoginPage />} />
           <Route path="/PersonalPage" element={<PersonalPage />} />
-          <Route path="/friend" element={<FriendPage />} />
-          <Route path="/CreateProgessStep" element={<CreateProgessStep />} />
+          <Route path="/Friend" element={<FriendPage />} />
+          <Route path="/CreateProcessStep" element={<CreateProcessStep />} />
+          <Route path="/Statistic1" element={<Statistic1 />} />
+          <Route path="/UserGrowthChart" element={<UserGrowthChart />} />
+          <Route path="/MapChart" element={<MapChart />} />
           <Route path="/Service" element={<ServicePage />} />
           <Route path="/Group" element={<PostGroupPage />} />
+          <Route path="/UpdateProfile" element={<UpdateProfile />} />
+          <Route path="/UserFriends" element={<UserFriends />} />
+          <Route path="/ProcessList" element={<ProcessListPage />} />
+          <Route path="/ProcessResult" element={<ProcessResultPage />} />
+          <Route path="/WaitingOrderList" element={<WaitingListPage />} />
+          <Route path="/SavedPostPage" element={<SavedPostPage />} />
+          <Route path="/CreateService" element={<CreateServicePage />} />
+          <Route path="/ServiceDetail" element={<ServiceDetailPage />} />
+          <Route
+            path="/ProgressListFarmer"
+            element={<ProcessListFarmerPage />}
+          />
+          <Route path="/CreateStepPage" element={<CreateStepPage />} />
         </Routes>
       </Router>
-      <YourGroupCard />
     </div>
   );
 }
