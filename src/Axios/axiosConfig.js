5072a7a7301dcebf519df01512287bebbc6ecf5e
tranphuongnamstudy <namtpce170126@fpt.edu.vn>
2025-06-09 13:19:57 +0700

    Fix UI: fix bug scroll in header.css

--- src/Axios/axiosConfig.js
+++ src/Axios/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
-  timeout: 10000,
+  timeout: 50000,
   headers: {
     "Content-Type": "application/json",
   },
